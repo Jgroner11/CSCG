fixed bug and added normalization to propogation algorithms

heatmap.py
@@ -1,11 +1,55 @@
 import numpy as np
-from chmm_actions import CHMM, forwardE, datagen_structured_obs_room
 import matplotlib.pyplot as plt
 import igraph
 import matplotlib
 from matplotlib import cm, colors
 import matplotlib.image as mpimg
 import sys, os, pickle
+import math
+
+from chmm_actions import CHMM, forwardE, datagen_structured_obs_room
+from CSCG_helpers import Plotting, Reasoning
+
+def save_image(t, file, rotation=0):
+    V = mess_fwd[t]
+    graph = Plotting.plot_heat_map(
+        chmm, x, a, V, output_file=file, rotation=rotation
+    )
+
+def plot_path(start = None, plot_location=True, rotation = 0):
+    if start is None:
+        start = 0
+    end = len(mess_fwd)
+    img_path = "figures\\plot_fig.png"
+    save_image(start, img_path, rotation=rotation)
+    image = mpimg.imread(img_path)
+    fig, ax = plt.subplots()
+    ax.axis('off')
+    ax.set_title(f'mess_fwd activity at t={start}')
+    img_display = ax.imshow(image, cmap='viridis')
+    cbar = plt.colorbar(img_display, ax=ax, orientation='vertical')
+    if plot_location:
+        location_fig, location_ax, text = Plotting.plot_room(room, pos=(rc[start, 0], rc[start, 1]), t=start)
+    t = start
+    def update_image(event):
+        """Updates the plot with the next image when the specified key is pressed."""
+        nonlocal t, text, location_ax
+        if event.key == 'n' or event.key == 'b':  # 'n' key for next time step, 'b' key to back one time step 
+            if event.key == 'n' and t < end:
+                t += 1
+            elif event.key == 'b' and t > 0:
+                t -= 1
+            save_image(t, img_path, rotation=rotation)
+            new_image = mpimg.imread(img_path)
+            img_display.set_data(new_image)
+            ax.set_title(f'mess_fwd activity at t={t}')
+            fig.canvas.draw()
+            if plot_location:
+                text = Plotting.redraw_room(location_fig, location_ax, (rc[t, 0], rc[t, 1]), old_text=text, t=t)
+
+    fig.canvas.mpl_connect('key_press_event', update_image)
+    location_fig.canvas.mpl_connect('key_press_event', update_image)
+    plt.show()
 
 retrain_models = False
 
@@ -26,102 +70,8 @@ custom_colors = (
     / 256
 )
 
-def plot_graph(
-    chmm, x, a, output_file, cmap=cm.Spectral, multiple_episodes=False, vertex_size=30
-):
-    global n_clones
-    states = chmm.decode(x, a)[1]
+Plotting.custom_colors = custom_colors
 
-    v = np.unique(states)
-    if multiple_episodes:
-        T = chmm.C[:, v][:, :, v][:-1, 1:, 1:]
-        v = v[1:]
-    else:
-        T = chmm.C[:, v][:, :, v]
-    A = T.sum(0)
-    A /= A.sum(1, keepdims=True)
-
-    g = igraph.Graph.Adjacency((A > 0).tolist())
-    node_labels = np.arange(x.max() + 1).repeat(n_clones)[v]
-    if multiple_episodes:
-        node_labels -= 1
-    colors = [cmap(nl)[:3] for nl in node_labels / node_labels.max()]
-    out = igraph.plot(
-        g,
-        output_file,
-        layout=g.layout("kamada_kawai"),
-        vertex_color=colors,
-        vertex_label=v,
-        vertex_size=vertex_size,
-        margin=50,
-    )
-
-    return out
-
-def get_mess_fwd(chmm, x, pseudocount=0.0, pseudocount_E=0.0):
-    n_clones = chmm.n_clones
-    E = np.zeros((n_clones.sum(), len(n_clones)))
-    last = 0
-    for c in range(len(n_clones)):
-        E[last : last + n_clones[c], c] = 1
-        last += n_clones[c]
-    E += pseudocount_E
-    norm = E.sum(1, keepdims=True)
-    norm[norm == 0] = 1
-    E /= norm
-    T = chmm.C + pseudocount
-    norm = T.sum(2, keepdims=True)
-    norm[norm == 0] = 1
-    T /= norm
-    T = T.mean(0, keepdims=True)
-    log2_lik, mess_fwd = forwardE(
-        T.transpose(0, 2, 1), E, chmm.Pi_x, chmm.n_clones, x, x * 0, store_messages=True
-    )
-    return mess_fwd
-
-def plot_heat_map(
-    chmm, x, a, V, output_file, multiple_episodes=False, vertex_size=30
-):
-    # States is a list of which latent node (ie state) is most active at each time step
-    states = chmm.decode(x, a)[1]
-
-    v = np.unique(states)
-    if multiple_episodes:
-        T = chmm.C[:, v][:, :, v][:-1, 1:, 1:]
-        v = v[1:]
-    else:
-        T = chmm.C[:, v][:, :, v]
-    A = T.sum(0)
-    A /= A.sum(1, keepdims=True)
-    # A is a transition matrix of only the latent nodes (states) that get activated during walk of path
-
-    # V_displayed represents the activity of all the nodes that are present in the A matrix/graph based on the inputted V activity for all the nodes
-    V_displayed_nodes = np.zeros(v.shape)
-    for i, id in enumerate(v):
-        V_displayed_nodes[i] = V[id]
-
-    # Rn skip normalizing step
-    V_disp_norm = (V_displayed_nodes - np.min(V_displayed_nodes)) / (np.max(V_displayed_nodes) - np.min(V_displayed_nodes))
-    # V_disp_norm = V_displayed_nodes
-
-    # colormap = cm.get_cmap('viridis')
-    colormap = matplotlib.colormaps['viridis']
-    colors = colormap(V_disp_norm)
-    colors = [tuple(c) for c in colors]
-
-    g = igraph.Graph.Adjacency((A > 0).tolist())
-
-    out = igraph.plot(
-        g,
-        output_file,
-        layout=g.layout("kamada_kawai"),
-        vertex_color=colors,
-        vertex_label=v,
-        vertex_size=vertex_size,
-        margin=50,
-    )
-
-    return out
 
 simple_granular_room = np.array(
     [[4, 2, 4, 0],
@@ -129,16 +79,27 @@ simple_granular_room = np.array(
     [4, 1, 3, 0],
     [3, 3, 2, 0]]
 )
-n_emissions = np.max(simple_granular_room) + 1
+granular_room = np.array(
+    [
+        [4, 2, 3, 0, 3, 4, 4, 4],
+        [4, 4, 3, 2, 3, 2, 3, 4],
+        [4, 4, 2, 0, 4, 2, 4, 0],
+        [0, 2, 4, 4, 3, 0, 0, 2],
+        [3, 3, 4, 0, 4, 1, 3, 0],
+        [2, 4, 2, 3, 3, 3, 2, 0],
+    ]
+)
+room = simple_granular_room
+name = 'navigation-simple_granular_room'
+
+n_emissions = np.max(room) + 1
 c = np.zeros((n_emissions+1, 3))
 c[:n_emissions] = custom_colors[:n_emissions]
 
-
-a, x, rc = datagen_structured_obs_room(simple_granular_room, length=5000)
+a, x, rc = datagen_structured_obs_room(room, length=5000)
 
 n_clones = np.ones(n_emissions, dtype=np.int64) * 25
 
-name = 'navigation-simple_granular_room'
 file = os.path.join("models", f"{name}.pkl")
 if os.path.isfile(file) and not retrain_models:
     with open(file, 'rb') as f:
@@ -154,13 +115,11 @@ chmm.learn_viterbi_T(x, a, n_iter=100)
 
 # Plot the layout of the room
 cmap = colors.ListedColormap(c[:n_emissions])
-plt.matshow(simple_granular_room, cmap=cmap)
-plt.title('Figure 1: Room Layout')
-plt.savefig("figures/granular_room.pdf")
 
+# Plot the learned graph
 file = os.path.join("figures", f"{name}.png")
-graph = plot_graph(
-    chmm, x, a, output_file=file, cmap=cmap
+graph = Plotting.plot_graph(
+    chmm, x, a, output_file=file, cmap=cmap, rotation=3
 )
 
 image = mpimg.imread(file)
@@ -168,64 +127,7 @@ fig, ax = plt.subplots()
 ax.axis('off')
 ax.imshow(image)
 
-def save_image(t, file):
-    V = mess_fwd[t]
-    graph = plot_heat_map(
-        chmm, x, a, V, output_file=file
-    )
-    
 
-def plot_path(start = None):
-    if start is None:
-        start = 0
-    end = len(mess_fwd)
-    img_path = "figures\\plot_fig.png"
-    save_image(start, img_path)
-    image = mpimg.imread(img_path)
-    fig, ax = plt.subplots()
-    ax.axis('off')
-    img_display = ax.imshow(image)
-    t = start + 1
-    def update_image(event):
-        """Updates the plot with the next image when the specified key is pressed."""
-        nonlocal t
-        if event.key == 'n':  # 'n' key for next image
-            if t >= end:
-                plt.close(fig)
-            else:
-                save_image(t, img_path)
-                new_image = mpimg.imread(img_path)
-                img_display.set_data(new_image)
-                fig.canvas.draw()
-                t += 1
+mess_fwd = Reasoning.get_mess_fwd(chmm, x, pseudocount_E=0.1)
+plot_path(0, rotation = 3)
 
-    fig.canvas.mpl_connect('key_press_event', update_image)
-    plt.show()
-
-mess_fwd = get_mess_fwd(chmm, x, pseudocount_E=0.1)
-plot_path(210)
-
-# sequential colormaps:
-# viridis
-# plasma
-# inferno
-# magma
-# cividis
-# Blues
-# BuGn
-# BuPu
-# GnBu
-# Greens
-# Greys
-# Oranges
-# OrRd
-# PuBu
-# PuBuGn
-# PuRd
-# Purples
-# RdPu
-# Reds
-# YlGn
-# YlGnBu
-# YlOrBr
-# YlOrRd

heatmap2.py
@@ -1,11 +1,48 @@
 import numpy as np
-from chmm_actions import CHMM, forwardE, datagen_structured_obs_room
 import matplotlib.pyplot as plt
 import igraph
 import matplotlib
 from matplotlib import cm, colors
 import matplotlib.image as mpimg
 import sys, os, pickle
+import math
+
+from chmm_actions import CHMM, forwardE, datagen_structured_obs_room
+from CSCG_helpers import Plotting, Reasoning
+
+def plot_reasoning(seq):
+    mess_fwd = Reasoning.get_mess_fwd(chmm, seq, pseudocount_E=0.1)
+    V_init = mess_fwd[-1]
+    img_path = "figures\\reasoning_fig.png"
+    graph = Plotting.plot_heat_map(
+        chmm, x, a, V_init, output_file=img_path
+    )
+    image = mpimg.imread(img_path)
+    fig, ax = plt.subplots()
+    ax.axis('off')
+    t = 0
+    ax.set_title(f't={t}')
+    img_display = ax.imshow(image, cmap='viridis')
+    cbar = plt.colorbar(img_display, ax=ax, orientation='vertical')
+
+    V = V_init
+    def update_image(event):
+        """Updates the plot with the next image when the specified key is pressed."""
+        nonlocal V, t
+        if event.key == 'n':  # 'n' key for next image
+            V = Reasoning.forwardV(V, V_init, chmm.T)
+            t += 1
+            ax.set_title(f't={t}')
+            print(sum(V_init), sum(V))
+            graph = Plotting.plot_heat_map(
+                chmm, x, a, V, output_file=img_path
+            )
+            new_image = mpimg.imread(img_path)
+            img_display.set_data(new_image)
+            fig.canvas.draw()
+
+    fig.canvas.mpl_connect('key_press_event', update_image)
+    plt.show()
 
 retrain_models = False
 
@@ -26,101 +63,7 @@ custom_colors = (
     / 256
 )
 
-def plot_graph(
-    chmm, x, a, output_file, cmap=cm.Spectral, multiple_episodes=False, vertex_size=30
-):
-    global n_clones
-    states = chmm.decode(x, a)[1]
-
-    v = np.unique(states)
-    if multiple_episodes:
-        T = chmm.C[:, v][:, :, v][:-1, 1:, 1:]
-        v = v[1:]
-    else:
-        T = chmm.C[:, v][:, :, v]
-    A = T.sum(0)
-    A /= A.sum(1, keepdims=True)
-
-    g = igraph.Graph.Adjacency((A > 0).tolist())
-    node_labels = np.arange(x.max() + 1).repeat(n_clones)[v]
-    if multiple_episodes:
-        node_labels -= 1
-    colors = [cmap(nl)[:3] for nl in node_labels / node_labels.max()]
-    out = igraph.plot(
-        g,
-        output_file,
-        layout=g.layout("kamada_kawai"),
-        vertex_color=colors,
-        vertex_label=v,
-        vertex_size=vertex_size,
-        margin=50,
-    )
-
-    return out
-
-def get_mess_fwd(chmm, x, pseudocount=0.0, pseudocount_E=0.0):
-    n_clones = chmm.n_clones
-    E = np.zeros((n_clones.sum(), len(n_clones)))
-    last = 0
-    for c in range(len(n_clones)):
-        E[last : last + n_clones[c], c] = 1
-        last += n_clones[c]
-    E += pseudocount_E
-    norm = E.sum(1, keepdims=True)
-    norm[norm == 0] = 1
-    E /= norm
-    T = chmm.C + pseudocount
-    norm = T.sum(2, keepdims=True)
-    norm[norm == 0] = 1
-    T /= norm
-    T = T.mean(0, keepdims=True)
-    log2_lik, mess_fwd = forwardE(
-        T.transpose(0, 2, 1), E, chmm.Pi_x, chmm.n_clones, x, x * 0, store_messages=True
-    )
-    return mess_fwd
-
-def plot_heat_map(
-    chmm, x, a, V, output_file, multiple_episodes=False, vertex_size=30
-):
-    # States is a list of which latent node (ie state) is most active at each time step
-    states = chmm.decode(x, a)[1]
-
-    v = np.unique(states)
-    if multiple_episodes:
-        T = chmm.C[:, v][:, :, v][:-1, 1:, 1:]
-        v = v[1:]
-    else:
-        T = chmm.C[:, v][:, :, v]
-    A = T.sum(0)
-    A /= A.sum(1, keepdims=True)
-    # A is a transition matrix of only the latent nodes (states) that get activated during walk of path
-
-    # V_displayed represents the activity of all the nodes that are present in the A matrix/graph based on the inputted V activity for all the nodes
-    V_displayed_nodes = np.zeros(v.shape)
-    for i, id in enumerate(v):
-        V_displayed_nodes[i] = V[id]
-
-    # Rn skip normalizing step
-    V_disp_norm = (V_displayed_nodes - np.min(V_displayed_nodes)) / (np.max(V_displayed_nodes) - np.min(V_displayed_nodes))
-    # V_disp_norm = V_displayed_nodes
-
-    colormap = matplotlib.colormaps['viridis']
-    colors = colormap(V_disp_norm)
-    colors = [tuple(c) for c in colors]
-
-    g = igraph.Graph.Adjacency((A > 0).tolist())
-
-    out = igraph.plot(
-        g,
-        output_file,
-        layout=g.layout("kamada_kawai"),
-        vertex_color=colors,
-        vertex_label=v,
-        vertex_size=vertex_size,
-        margin=50,
-    )
-
-    return out
+Plotting.custom_colors = custom_colors
 
 simple_granular_room = np.array(
     [[4, 2, 4, 0],
@@ -128,16 +71,29 @@ simple_granular_room = np.array(
     [4, 1, 3, 0],
     [3, 3, 2, 0]]
 )
-n_emissions = np.max(simple_granular_room) + 1
+granular_room = np.array(
+    [
+        [4, 2, 3, 0, 3, 4, 4, 4],
+        [4, 4, 3, 2, 3, 2, 3, 4],
+        [4, 4, 2, 0, 4, 2, 4, 0],
+        [0, 2, 4, 4, 3, 0, 0, 2],
+        [3, 3, 4, 0, 4, 1, 3, 0],
+        [2, 4, 2, 3, 3, 3, 2, 0],
+    ]
+)
+
+room = granular_room
+name = 'navigation-granular_room'
+
+n_emissions = np.max(room) + 1
 c = np.zeros((n_emissions+1, 3))
 c[:n_emissions] = custom_colors[:n_emissions]
 
 
-a, x, rc = datagen_structured_obs_room(simple_granular_room, length=5000)
+a, x, rc = datagen_structured_obs_room(room, length=5000)
 
 n_clones = np.ones(n_emissions, dtype=np.int64) * 25
 
-name = 'navigation-simple_granular_room'
 file = os.path.join("models", f"{name}.pkl")
 if os.path.isfile(file) and not retrain_models:
     with open(file, 'rb') as f:
@@ -153,12 +109,12 @@ chmm.learn_viterbi_T(x, a, n_iter=100)
 
 # Plot the layout of the room
 cmap = colors.ListedColormap(c[:n_emissions])
-plt.matshow(simple_granular_room, cmap=cmap)
+plt.matshow(room, cmap=cmap)
 plt.title('Figure 1: Room Layout')
 plt.savefig("figures/granular_room.pdf")
 
 file = os.path.join("figures", f"{name}.png")
-graph = plot_graph(
+graph = Plotting.plot_graph(
     chmm, x, a, output_file=file, cmap=cmap
 )
 
@@ -167,74 +123,5 @@ fig, ax = plt.subplots()
 ax.axis('off')
 ax.imshow(image)
 
-def updateV(V, V_init, T):
-        s = V_init
-        for i in range(T.shape[0]):
-            s += V @ T[i]
-        return s
+plot_reasoning(x[1690:1700])
 
-def plot_reasoning(seq):
-    mess_fwd = get_mess_fwd(chmm, seq, pseudocount_E=0.1)
-    V_init = mess_fwd[-1]
-    img_path = "figures\\reasoning_fig.png"
-    graph = plot_heat_map(
-        chmm, x, a, V_init, output_file=img_path
-    )
-    image = mpimg.imread(img_path)
-    fig, ax = plt.subplots()
-    ax.axis('off')
-    img_display = ax.imshow(image, cmap='viridis')
-    cbar = plt.colorbar(img_display, ax=ax, orientation='vertical')
-
-    V = V_init
-    def update_image(event):
-        """Updates the plot with the next image when the specified key is pressed."""
-        nonlocal V
-        if event.key == 'n':  # 'n' key for next image
-            V = updateV(V, V_init, chmm.T)
-            print(max(V))
-            graph = plot_heat_map(
-                chmm, x, a, V, output_file=img_path
-            )
-            new_image = mpimg.imread(img_path)
-            img_display.set_data(new_image)
-            fig.canvas.draw()
-
-    fig.canvas.mpl_connect('key_press_event', update_image)
-    plt.show()
-
-
-plot_reasoning(x[200:300])
-
-def forward_search(chmm, x, n_iters=10):
-    mess_fwd = get_mess_fwd(chmm, x, pseudocount_E=0.1)
-    V_init = mess_fwd[-1]
-    V = V_init
-    for i in range(n_iters):
-        V = updateV(V, V_init, chmm.C)
-
-
-# sequential colormaps:
-# viridis
-# plasma
-# inferno
-# magma
-# cividis
-# Blues
-# BuGn
-# BuPu
-# GnBu
-# Greens
-# Greys
-# Oranges
-# OrRd
-# PuBu
-# PuBuGn
-# PuRd
-# Purples
-# RdPu
-# Reds
-# YlGn
-# YlGnBu
-# YlOrBr
-# YlOrRd