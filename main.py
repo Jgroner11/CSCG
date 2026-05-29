c2f443e0de467cbf7bb3aff843cce2190601cd14
Jacob Groner <jacobgroner@gmail.com>
2026-05-29 11:12:06 -0700

    new experiments added, models no longer tracked by github

--- main.py
+++ main.py
@@ -5,43 +5,41 @@ import matplotlib.pyplot as plt
 
 from CSCG_helpers import Plotting, Reasoning
 from experiment import Experiment
-from rooms import DONUT
+from rooms import DONUT, GRANULAR_ROOM, SIMPLE_GRANULAR_ROOM
 
 # for experiment_name in Experiment.list_experiments():
 #     print(f"- {experiment_name}")
 
 
-exp = Experiment(
-    name="asymmetric_loop",
-    graph="loop",
-    graph_normalize=True,
-    graph_third_column="weight",
-    plan_method=Reasoning.STP,
-    starts=[0],
-    targets=[3],
-    wavefront_steps=4,
-    planning_steps=4,
-)
+# exp = Experiment(
+#     name="granular_room",
+#     room = GRANULAR_ROOM,
+#     plan_method=Reasoning.STP,
+#     starts=[0],
+#     targets=[3],
+#     wavefront_steps=4,
+#     planning_steps=4,
+# )
 
-exp.run()
+# exp.run()
 
-output_file = os.path.join("figures", f"{exp.name}-graph.png")
-Plotting.plot_graph(
-    exp.model,
-    exp.observations,
-    exp.actions,
-    output_file=output_file,
-    states=exp.decoded_states,
-    flip = True,
-    rotation=.9
-)
-image = mpimg.imread(output_file)
-fig, ax = plt.subplots()
-ax.axis("off")
-ax.imshow(image)
-plt.show()
+# output_file = os.path.join("figures", f"{exp.name}-graph.png")
+# Plotting.plot_graph(
+#     exp.model,
+#     exp.observations,
+#     exp.actions,
+#     output_file=output_file,
+#     states=exp.decoded_states,
+#     flip = True,
+#     rotation=.9
+# )
+# image = mpimg.imread(output_file)
+# fig, ax = plt.subplots()
+# ax.axis("off")
+# ax.imshow(image)
+# plt.show()
 
-exp.visualize(mode="combined")
+# exp.visualize(mode="combined")
 
 
 
